bd336a5a7ca948716cebe2735c9164b15c6a8fd3
Matt Megarry <mattmegarry@gmail.com>
2023-09-12 16:31:14 +0100

    [feature] - crumbs are displayed nicely in the crumb list view on admin site

--- headcrumbs/apps/crumbs/admin.py
+++ headcrumbs/apps/crumbs/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
 from .models import Crumb
 
-admin.site.register(Crumb)
+class CrumbAdmin(admin.ModelAdmin):
+    model = Crumb
+    list_display = ('text_truncated', 'url_truncated', 'user')
+    list_filter = ('user',)
+
+    def text_truncated(self, obj):
+        return obj.text[:100]
+    text_truncated.short_description = 'Text'
+    
+    def url_truncated(self, obj):
+        return obj.url[:50]
+    url_truncated.short_description = 'URL'
+
+admin.site.register(Crumb, CrumbAdmin)
